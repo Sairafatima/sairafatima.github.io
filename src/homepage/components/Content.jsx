d2e8a5f774eef2058792f51ecd3c9479a587aac3
sairafatima <saira030854@gmail.com>
2023-09-15 13:56:23 +0500

    updates

--- src/homepage/components/Content.jsx
+++ src/homepage/components/Content.jsx
@@ -6,6 +6,12 @@ function Content(props) {
     <div className="content-main-container">
       <SideProfile />
       <ProfileData tab={props.currentTab} />
+      <div className="credits-div">
+        <p>Design Template :&nbsp;</p>
+        <a href={"https://www.figma.com/community/file/1264680769254941322/Portfolio-Design"} target={"_blank"}>
+          By Beatrice Wambui Mbugua
+        </a>
+      </div>
     </div>
   );
 }
