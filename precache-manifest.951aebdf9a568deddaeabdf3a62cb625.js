9b17d89feee23e43692724a1b880096e77451596
Shahzil-Fatima <shahzilfatima.0.1@gmail.com>
2026-01-07 19:03:16 +0500

    updates

---
+++ precache-manifest.951aebdf9a568deddaeabdf3a62cb625.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a331bc6339baab737c09aeb9d3c87b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "40fdcb937b447ce8428f",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "687528004117f7ff00ba",
+    "url": "./static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "40fdcb937b447ce8428f",
+    "url": "./static/js/2.f7e20445.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.f7e20445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687528004117f7ff00ba",
+    "url": "./static/js/main.0baf4839.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  },
+  {
+    "revision": "05aac4e5a34a0a74b97cad31d7ff1a1f",
+    "url": "./static/media/my_profile.05aac4e5.jpg"
+  },
+  {
+    "revision": "2d6da1ee2627dea903b01ff3d4e20638",
+    "url": "./static/media/project.2d6da1ee.jpg"
+  }
+]);
